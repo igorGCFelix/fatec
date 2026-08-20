3f00c748d67c3268738dd3fdc358c8e94aed5b24
Igor Felix <74800549+igorGCFelix@users.noreply.github.com>
2026-08-20 12:16:05 -0300

    Add files via upload

---
+++ Linguagem de Programação/aula1/aula-01.py
@@ -0,0 +1,24 @@
+# print ('Olá, mundo!')
+
+# print(3+3)
+# print(4-2)
+# print(2**4)
+# print(10/2)
+# print(10//3)
+# print(10%3)
+# print(3+4.0)
+# # print(3+"3") num dá
+
+# nome_colaborador = "Igor"
+# idade_colaborador = 18
+# altura_colaborador = 1.68
+# status_colaborador = True
+
+# print("Nome:", nome_colaborador)
+# print("Nome: ", nome_colaborador)
+# print(f"Nome: { nome_colaborador }")
+# print(type(nome_colaborador))
+
+idade_colaborador = int(input("Qual a sua idade?\n"))
+print("Sua idade é de",idade_colaborador)
+print(type(idade_colaborador))
