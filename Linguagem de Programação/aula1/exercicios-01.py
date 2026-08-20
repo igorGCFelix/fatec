f1d5ec453daca3e9bbc46fdbef9f4a11896de3b6
Igor Felix <igorfelixbob@gmail.com>
2026-08-20 20:35:46 -0300

    exercicios aula 1

--- Linguagem de Programação/aula1/exercicios-01.py
+++ Linguagem de Programação/aula1/exercicios-01.py
@@ -1,7 +1,20 @@
 # 1
+print ("--- EXERCÍCIO 1 ---")
 nome = input("Digite o seu nome:\n")
-print ("Olá, ",nome)
+print ("Olá, ",nome, "\n")
 
 # 2
-nota1 = input("Digite a primeira nota: \n")
-nota2 = input("Digite a segunda nota: \n")
+print ("--- EXERCÍCIO 2 ---")
+# não está falando se a nota é até 10
+a1 = float(input("Digite a primeira nota: \n"))
+a2 = float(input("Digite a segunda nota: \n"))
+media = (a1+a2)/2
+print ("A média do aluno é ",media, "\n")
+
+#3
+print ("--- EXERCÍCIO 3 ---")
+#não falou se existe ou n desconto de 100%
+preco = float(input("Digite o preco do produto: \n"))
+desconto = float(input("Digite o percentual de desconto do produto: \n"))
+valorFinal = preco - (desconto*preco/100)
+print (f"O valor final do produto é: R${valorFinal}")
