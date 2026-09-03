c378253c67e22c90026244f8e2af2ad77245ddee
Igor Felix <74800549+igorGCFelix@users.noreply.github.com>
2026-09-03 12:01:45 -0300

    Add files via upload

--- Linguagem de Programação/aula3/exercicios-03.py
+++ Linguagem de Programação/aula3/exercicios-03.py
@@ -1,4 +1,4 @@
-# #1
+#1
 print("---Exercício 1---")
 print("---for---")
 for i in range (10, 0, -1):
@@ -11,7 +11,7 @@ while i >= 1:
         print(i)
     i = i - 1 
 
-# #2
+#2
 print("---Exercício 2---")
 print("---for---")
 for i in range(1, 6):
@@ -38,7 +38,7 @@ while i < 10:
 #4
 print("---Exercício 4---")
 numero = int(input("Digite um número inteiro e positivo: \n"))
-if numero < 0:
+if numero <= 0:
     print("Erro! Digite um número positivo!")
 else:
     print("---for---")
@@ -50,3 +50,31 @@ else:
     while i < 10:
         print(numero*i)
         i = i + 1
+
+# 5
+print("---Exercício 5---")
+i = 0
+soma = 0
+while i <= 9:
+    numero = float(input("Digite um número positivo: \n"))
+    if numero > 0:
+        soma = soma + numero
+        i = i + 1
+    else:
+        print("O número não é positivo! Digite novamente")
+print("A soma dos 10 números positivos digitados é: ",soma)
+
+#6
+print("---Exercício 6---")
+i = 0
+soma = 0
+while i <= 9:
+    numero = float(input("Digite um número positivo: \n"))
+    if numero > 0:
+        soma = soma + numero
+        i = i + 1
+    elif numero == 0:
+        i=10
+    else:
+        print("O número não é positivo! Digite novamente")
+print("A soma dos 10 números positivos digitados é: ",soma)
