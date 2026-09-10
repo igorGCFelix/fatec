ad378eb997aad851938b86b765015c7ff1e68907
Igor Felix <74800549+igorGCFelix@users.noreply.github.com>
2026-09-10 12:09:54 -0300

    Add files via upload

--- Linguagem de Programação/aula4/exercicios-04.py
+++ Linguagem de Programação/aula4/exercicios-04.py
@@ -1,4 +1,5 @@
 #1
+print("---Exercício 1 ---")
 # lista de alunos e médias
 # alunos = []
 # medias = []
@@ -24,26 +25,45 @@
 #             print("Escolha entre a opção (S/N)!")
 
 #2
-alunos = []
-medias = []
+print("---Exercício 2 ---")
+# alunos = []
+# medias = []
 
-i = 0
-while i < 1: 
-    aluno = input("Digite o nome do aluno: \n")
-    alunos.append(aluno)
-    media = input("Digite a média do aluno: \n")
-    medias.append(media)
+# i = 0
+# while i < 1: 
+#     aluno = input("Digite o nome do aluno: \n")
+#     alunos.append(aluno)
+#     media = input("Digite a média do aluno: \n")
+#     medias.append(media)
 
-    a=0
-    while a < 1:
-        continuar=input("Deseja continuar adicionando notas? (S/N): \n")
-        if continuar.upper() == "N":
-            alunoX=input("Digite o nome do aluno que você quer ver a média: \n")
-            idx = alunos.index(alunoX)
-            print(f"Aluno: {alunos[idx]}, Média: {medias[idx]}")
-            a = 1
-            i = 1
-        elif continuar.upper() == "S":
-            a=1
-        else:
-            print("Escolha entre a opção (S/N)!")
+#     a=0
+#     while a < 1:
+#         continuar=input("Deseja continuar adicionando notas? (S/N): \n")
+#         if continuar.upper() == "N":
+#             alunoX=input("Digite o nome do aluno que você quer ver a média: \n")
+#             if alunoX not in alunos:
+#                 print("Não há um aluno com esse nome. Tabela de alunos existentes e suas médias:")
+#                 for item in range(len(alunos)):
+#                     print(f"Aluno: {alunos[item]}, Média: {medias[item]}")
+#                 a = 1
+#                 i = 1
+
+#             else:        
+#                 idx = alunos.index(alunoX)
+#                 print(f"Aluno: {alunos[idx]}, Média: {medias[idx]}")
+#                 a = 1
+#                 i = 1
+#         elif continuar.upper() == "S":
+#             a=1
+#         else:
+#             print("Escolha entre a opção (S/N)!")
+
+#3
+# print("---Exercício 3 ---")
+# nomes = []
+# medias = []
+
+# # nomes = [ "Ana", "Claudia", "Diego", "Diogo", "Elizia", "Fabricio", "Gabriella", "Marcelo", "Marcelly", "Tássia" ]
+# # medias = [ 8.5, 6.0, 4.5, 6.5, 9.5, 5.5, 8.0, 4.0, 9.0, 2.5]
+
+
