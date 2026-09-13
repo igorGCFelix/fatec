37c1d256ed68c296baabb9edb1cdf8e88061608c
Igor Felix <igorfelixbob@gmail.com>
2026-09-12 22:19:43 -0300

    ta tudo certinho, pode comemorar tropa

--- Linguagem de Programação/aula4/exercicios-04.py
+++ Linguagem de Programação/aula4/exercicios-04.py
@@ -1,63 +1,61 @@
-
 #1
-print("---Exercício 1 ---")
-# lista de alunos e médias
-# alunos = []
-# medias = []
+print("--- Exercício 1 ---")
+alunos = []
+medias = []
 
-# i = 0
-# while i < 1: 
-#     aluno = input("Digite o nome do aluno: \n")
-#     alunos.append(aluno)
-#     media = input("Digite a média do aluno: \n")
-#     medias.append(media)
+i = 0
+while i < 1: 
+    aluno = input("Digite o nome do aluno: \n")
+    alunos.append(aluno)
+    media = input("Digite a média do aluno: \n")
+    medias.append(media)
 
-#     a=0
-#     while a < 1:
-#         continuar=input("Deseja continuar adicionando notas? (S/N): \n")
-#         if continuar.upper() == "N":
-#             for item in range(len(alunos)):
-#                 print(f"Aluno: {alunos[item]}, Média: {medias[item]}")
-#             a = 1
-#             i = 1
-#         elif continuar.upper() == "S":
-#             a=1
-#         else:
-#             print("Escolha entre a opção (S/N)!")
+    a=0
+    while a < 1:
+        continuar=input("Deseja continuar adicionando notas? (S/N): \n")
+        if continuar.upper() == "N":
+            for item in range(len(alunos)):
+                print(f"Aluno: {alunos[item]}, Média: {medias[item]}")
+            a = 1
+            i = 1
+        elif continuar.upper() == "S":
+            a=1
+        else:
+            print("Escolha entre a opção (S/N)!")
 
 #2
-print("---Exercício 2 ---")
-# alunos = []
-# medias = []
+print("--- Exercício 2 ---")
+alunos = []
+medias = []
 
-# i = 0
-# while i < 1: 
-#     aluno = input("Digite o nome do aluno: \n")
-#     alunos.append(aluno)
-#     media = input("Digite a média do aluno: \n")
-#     medias.append(media)
+i = 0
+while i < 1: 
+    aluno = input("Digite o nome do aluno: \n")
+    alunos.append(aluno)
+    media = input("Digite a média do aluno: \n")
+    medias.append(media)
 
-#     a=0
-#     while a < 1:
-#         continuar=input("Deseja continuar adicionando notas? (S/N): \n")
-#         if continuar.upper() == "N":
-#             alunoX=input("Digite o nome do aluno que você quer ver a média: \n")
-#             if alunoX not in alunos:
-#                 print("Não há um aluno com esse nome. Tabela de alunos existentes e suas médias:")
-#                 for item in range(len(alunos)):
-#                     print(f"Aluno: {alunos[item]}, Média: {medias[item]}")
-#                 a = 1
-#                 i = 1
+    a=0
+    while a < 1:
+        continuar=input("Deseja continuar adicionando notas? (S/N): \n")
+        if continuar.upper() == "N":
+            alunoX=input("Digite o nome do aluno que você quer ver a média: \n")
+            if alunoX not in alunos:
+                print("Não há um aluno com esse nome. Tabela de alunos existentes e suas médias:")
+                for item in range(len(alunos)):
+                    print(f"Aluno: {alunos[item]}, Média: {medias[item]}")
+                a = 1
+                i = 1
 
-#             else:        
-#                 idx = alunos.index(alunoX)
-#                 print(f"Aluno: {alunos[idx]}, Média: {medias[idx]}")
-#                 a = 1
-#                 i = 1
-#         elif continuar.upper() == "S":
-#             a=1
-#         else:
-#             print("Escolha entre a opção (S/N)!")
+            else:        
+                idx = alunos.index(alunoX)
+                print(f"Aluno: {alunos[idx]}, Média: {medias[idx]}")
+                a = 1
+                i = 1
+        elif continuar.upper() == "S":
+            a=1
+        else:
+            print("Escolha entre a opção (S/N)!")
 
 #3
 def verificarIndice(tamanho):
@@ -75,54 +73,74 @@ def verificarIndice(tamanho):
             print("Digite um número inteiro")
         
 
-print("---Exercício 3 ---")
+print("--- Exercício 3 ---")
 nomes = [ "Ana", "Claudia", "Diego", "Diogo", "Elizia", "Fabricio", "Gabriella", "Marcelo", "Marcelly", "Tássia" ]
 medias = [ 8.5, 6.0, 4.5, 6.5, 9.5, 5.5, 8.0, 4.0, 9.0, 2.5]
 
-for item in range(len(nomes)):
-    print(f"Aluno: {nomes[item]}, Média: {medias[item]}, Índice: {item}")
+o=0
+while o<1:
+    print("--- Lista ---")
+    for item in range(len(nomes)):
+        print(f"Aluno: {nomes[item]}, Média: {medias[item]}, Índice: {item}")
+    opcao=input("Escolha entre uma das opções: [A]lterar, [E]xcluir ou [S]air \n")
+    # 3A
+    if opcao.upper() == "A":
+        #vai receber e verificar o indice
+        indice = verificarIndice(len(nomes))
 
-opcao=input("Escolha entre uma das opções: [A]lterar, [E]xcluir ou [S]air \n")
-# 3A
-if opcao.upper() == "A":
-    #vai receber e verificar o indice
-    indice = verificarIndice(len(nomes))
+        #recebendo os novos valores
+        nomeNovo=input("Digite o nome do aluno:")
+        mediaNova=float(input("Digite a média do aluno: "))
 
-    #recebendo os novos valores
-    nomeNovo=input("Digite o nome do aluno:")
-    mediaNova=float(input("Digite a média do aluno: "))
+        #confirmação das alterações
+        b=0
+        while b<1: 
+            resposta = input("Deseja confirmar essas alterações (S/N)?")
+            if resposta.upper() == "S":
+                medias[indice] = mediaNova
+                nomes[indice] = nomeNovo
+                print("Alterações feitas com sucesso!")
+                print(f"Alterações: Aluno {nomes[indice]}, Média {medias[indice]}")
+                b=1
+            elif resposta.upper() == "N":
+                print("Alterações canceladas!")
+                b=1
+            else:
+                print("Valor inválido, responda novamente.")
 
-    #confirmação das alterações
-    b=0
-    while b<1: 
-        resposta = input("Deseja confirmar essas alterações (S/N)?")
-        if resposta.upper() == "S":
-            medias[indice] = mediaNova
-            nomes[indice] = nomeNovo
-            print("Alterações feitas com sucesso!")
-            print(f"Alterações: Aluno {nomes[indice]}, Média {medias[indice]}")
-            b=1
-        elif resposta.upper() == "N":
-            print("Alterações canceladas!")
-            b=1
-        else:
-            print("Valor inválido, responda novamente.")
+    elif opcao.upper() == "E":
+        indice=verificarIndice(len(nomes))
 
-elif opcao.upper() == "E":
-    indice=verificarIndice(len(nomes))
+        #confirmação da exclusão
+        b=0
+        while b<1: 
+            resposta = input("Deseja confirmar essa exclusão (S/N)?")
+            if resposta.upper() == "S":
+                nomes.pop(indice)
+                medias.pop(indice)
+                print("Exclusão feita com sucesso!")
+                b=1
+            elif resposta.upper() == "N":
+                print("Exclusão cancelada!")
+                b=1
+            else:
+                print("Valor inválido, responda novamente.")
 
-    #confirmação da exclusão
-    b=0
-    while b<1: 
-        resposta = input("Deseja confirmar essa exclusão (S/N)?")
-        if resposta.upper() == "S":
-            nomes.pop(indice)
-            medias.pop(indice)
-            print("Exclusão feita com sucesso!")
-            b=1
-        elif resposta.upper() == "N":
-            print("Exclusão cancelada!")
-            b=1
-        else:
-            print("Valor inválido, responda novamente.")
+    elif opcao.upper() == "S":
+        c=0
+        while c<1:
+            resposta=input("Deseja sair? \n")
+            if resposta.upper()=="S":
+                for item in range(len(nomes)):
+                        print(f"Aluno: {nomes[item]}, Média: {medias[item]}, Índice: {item}")
+                print("Sistema finalizado.")
+                c=1
+                o=1
+            elif resposta.upper()=="N":
+                print("Voltando...")
+                c=1
+            else:
+                print("Valor inválido, responda novamente.")
 
+    else:
+        print("Valor inválido, digite novamente")
